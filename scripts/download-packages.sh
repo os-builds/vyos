324050d3718eb1fa852f0a4cef56b23714449699
Nico Felbinger <26925347+felbinger@users.noreply.github.com>
2023-01-01 22:51:29 +0100

    Update and rename download-frr_exporter to download-packages.sh

---
+++ scripts/download-packages.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env sh
+
+# this script downloads our vyatta packages from github.com/pkgs-hub
+declare -A packages=(
+  ["node_exporter"]="1.5.0"
+   ["frr_exporter"]="1.1.4"
+     ["zia-server"]="0.2.5"
+)
+
+for package in ${!packages[@]}; do
+  version=${packages[$package]}
+  url="https://github.com/pkgs-hub/vyatta-${package}/releases/download/v${version}/vyatta-${package}_${version}_amd64.deb"
+
+  echo "[${0}] Downloading ${url}..."
+  curl -s -L "${url}" -o "$(pwd)/$(basename "${url}")"
+
+  echo "[${0}] Adding ${output} to pending packages..."
+  echo "${output}" >>packages.txt
+done
