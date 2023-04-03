1ae353ac259e51560902074d8d4f31f4a1e0f32f
Marcel <me@m4rc3l.de>
2023-04-03 19:23:24 +0200

    Revert "Delete download-node_exporter.sh"
    This reverts commit 0f7d3fae7d28f58db2601b68857ea6b3fdb53700.

---
+++ scripts/download-node_exporter.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+
+version="1.5.0"
+url="https://github.com/pkgs-hub/vyatta-node_exporter/releases/download/v${version}/vyatta-node-exporter_${version}_amd64.deb"
+
+output="$(pwd)/$(basename "${url}")"
+
+echo "[${0}] Downloading ${url}..."
+curl -s -L "${url}" -o "${output}"
+
+echo "[${0}] Adding ${output} to pending packages..."
+echo "${output}" >>packages.txt
