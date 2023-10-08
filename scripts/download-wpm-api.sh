fb3dade58e0bcdad6c36ab9e28374c2b7ed9c7f9
Nico Felbinger <26925347+felbinger@users.noreply.github.com>
2023-10-08 23:22:10 +0200

    fixed copy paste error

--- scripts/download-wpm-api.sh
+++ scripts/download-wpm-api.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 version="1.0.0"
-url="https://github.com/secshellnet/wpm-api-vyos/releases/download/v${version}/scripts/wpm-api-vyos_${version}_amd64.deb"
+url="https://github.com/secshellnet/wpm-api-vyos/releases/download/v${version}/wpm-api-vyos_${version}_amd64.deb"
 
 output="$(pwd)/$(basename "${url}")"
 
